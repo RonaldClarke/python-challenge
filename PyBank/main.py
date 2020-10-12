9e4e5aa84ded2a72260de1390d789a4430ce4e09
Ronald Clarke <ronnie.clarke@utoronto.ca>
2020-10-12 19:45:56 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,11 +3,14 @@ import os
 filepath = os.path.join("/Users/ronaldclarke/Desktop/GitHub/python-challenge/PyBank/Resources/budget_data.csv")
 MonthCount = 0
 TotalProfLoss = 0
+ProfitLoss = []
+
 with open(filepath,"r") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")   
     next(csvreader,None) #skips the header
     for row in csvreader:
         MonthCount = MonthCount + 1
         TotalProfLoss = TotalProfLoss + int(row[1])
-
+        ProfitLoss.append(row[1])
+print(ProfitLoss)
 
